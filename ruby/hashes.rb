744c555e73768009e249eab0533e68016dd6ff26
Matt Grimm <m.m.grimm1@gmail.com>
2016-07-21 22:01:00 -0500

    Refactor user interface

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -23,21 +23,27 @@ client[:decor] = gets.chomp.to_s
 
 #Print the hash in a readable form
 
-puts "#{client[:name]} who is #{client[:age]} years old,and would like us to decorate their house with #{client[:decor]} theme.\n
+puts "\n#{client[:name]} who is #{client[:age]} years old, and would like us to decorate their house with #{client[:decor]} theme.\n
 Further, it is #{client[:children]} that we should design with children in mind."
 
 #create opportunity for worker to edit input
-puts "\nDo you want to change any of the previous entries: name, age, children, or decor? Enter no to finish"
+puts "\nDo you want to change any of the previous entries: name, age, children, or decor? Input the category or 'no' to finish"
+#allow the user to select which category they need. From code stand point the code works without this line.
+#will be my new key
+for_the_user = gets.chomp.to_sym
+
+puts "\nWhat would you like to change #{for_the_user} to?"
+#will be my new value
 update = gets.chomp.to_sym
 
-if update = "no"
+if update == "no"
   puts "Final Client data is:
 	Name: #{client[:name]}
 	Age: #{client[:age]}
 	Design with children in mind: #{client[:children]}
 	Decor: #{client[:decor]}"
 else
-  client[update.to_sym] = update
+  client[for_the_user.to_sym] = update
   puts "Final Client data is:
 	Name: #{client[:name]}
 	Age: #{client[:age]}
