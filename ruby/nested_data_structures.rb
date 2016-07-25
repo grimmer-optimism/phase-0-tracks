0d6441acda44d5c8d84b42ba5d6150eedcf405c5
Matt Grimm <m.m.grimm1@gmail.com>
2016-07-24 20:06:17 -0500

    Create menu hash containing nested hashes and arrays

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,27 @@
+menu = {
+  appetizers: {
+    soup: {
+      soup_list: ['Beer Cheese', 'Beef Chilli', 'Creamy Tomato'],
+      beer_cheese: 5,
+      beef_chilli: 5,
+      creamy_tomato: 3
+    }
+    house_salad: 3,
+    egg_rolls: 3
+  }
+
+  main_course: {
+    entree_list: ['Steak and potatoes', 'Chicken Pot Pie', 'Baked Salmon and Vegetables'],
+    steak_potatoes: 25,
+    chicken_pot_pie: 16,
+    baked_salmon: 21
+  }
+
+  dessert: {
+    dessert_list: ['Strawberry Cheesecake', 'Chocolate Cake', 'Ice Cream' ],
+    strawberry_cheesecake: 5,
+    chocolate_cake: 5,
+    
+  }
+
+}
