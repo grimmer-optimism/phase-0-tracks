ed7d6218a286552401061f46b2897a7c752f5965
Matt Grimm <m.m.grimm1@gmail.com>
2016-07-27 07:04:07 -0500

    Finish release 0

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+class Puppy
+
+#Species: Puppy
+
+#Characteristics
+#Puppy_eyes: cute
+#Ears: floppy
+#Size: small
+
+#Behavior
+#Bored: bark
+#Retrieve: frisbee
+#Trick: roll over
+
+end
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
