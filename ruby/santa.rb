e46ed8622f955321831ede34c8f4180cc5a6068b
Matt Grimm <m.m.grimm1@gmail.com>
2016-07-27 07:54:53 -0500

    Add birthday and get mad methods. Add age, ethnicity, and gender class variables

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,12 +1,12 @@
-class Sanata
-
-  def initialize(name, gender, ethnicity)
-    @name = name
+class Santa
+#had to remove name from initialize for new santa loop.
+  def initialize(gender, ethnicity)
+    @age = 0
     @gender = gender
     @ethnicity = ethnicity
-    @age = age
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-    puts "Initializing new Santa"
+    puts "Initializing new Santa..."
+
   end
 
   def speak
@@ -17,7 +17,40 @@ class Sanata
     puts "That was a good #{cookie}!"
   end
 
-  tim = Sanata.new("tim")
+  def celebrate_birthday
+    @age += 1
+  end
 
-  tim.speak
-  tim.eat_milk_and_cookies("snickerdoodle")
+  def get_mad_at(reindeer_name)
+    @reindeer_ranking.delete(reindeer_name)
+    @reindeer_ranking.push(reindeer_name)
+  end
+
+#Getter & Setter Methods
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
+  def gender
+    @gender
+  end
+
+  #tim = Santa.new("tim")
+
+  #tim.speak
+  #tim.eat_milk_and_cookies("snickerdoodle")
+end
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+20.times do
+	new_santa = Santa.new(example_genders.sample, example_ethnicities.sample)
+	age = Random.rand(0...140)
+	puts "This Santa is #{new_santa.gender} gender and #{new_santa.ethnicity}"
+end
