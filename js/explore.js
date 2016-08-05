9e9cdb1d4d6a71edd7027dca1da213318d82b442
Matt Grimm <m.m.grimm1@gmail.com>
2016-08-05 07:49:44 -0500

    Add to function reverse and driver code

--- js/explore.js
+++ js/explore.js
@@ -3,3 +3,21 @@
 // Found reverse function for arrays on MDN, but this doesn't bring the components back together
 // Concatate the array into a string for the final finalanswer
 // No implicit return so need to return answer
+
+function reverse(string) {
+	var splitarray = string.split("");
+
+	var seperatedreverse = splitarray.reverse();
+
+	var finalanswer = seperatedreverse.join("");
+
+	return finalanswer;
+}
+
+reverse("hello");
+
+var success = reverse("hello")
+
+if (1==1) {
+	return success;
+}
