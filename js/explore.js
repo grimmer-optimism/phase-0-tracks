68ade94d57fbad3aaf1a01ba919243de25ab0fa3
Matt Grimm <m.m.grimm1@gmail.com>
2016-08-05 07:49:11 -0500

    Create pseudocode

---
+++ js/explore.js
@@ -0,0 +1,5 @@
+// Create a function named 'reverse' that takes a string and
+// splits the string into individual components creating an array and gaining access to array functions.
+// Found reverse function for arrays on MDN, but this doesn't bring the components back together
+// Concatate the array into a string for the final finalanswer
+// No implicit return so need to return answer
