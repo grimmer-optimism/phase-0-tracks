294423d4ebc8747baf7141c02daf086092431269
Matt Grimm <m.m.grimm1@gmail.com>
2016-08-08 09:59:59 -0500

    Complete release 2

--- js/algos.js
+++ js/algos.js
@@ -40,3 +40,41 @@ function equal_objects(object1, object2) {
 console.log(equal_objects({name: "Steven", age: 54}, {name: "Tamir", age:54}))
 // return false
 console.log(equal_objects({name: "Matt", age: 28}, {name: "Erin", age: 26}))
+
+
+// 1 function to generate random words and another to build the new array of length determined by argument
+// create alphabet variable to use with random math function
+// create variable new_word to hold future string
+// for loop to create new string of length determined by the argument (number) provided
+// return the new word
+
+// Second function that calls on the first method to generate a new word
+// create empty array called new_array
+// for loop to iterate through until quota met which is set by argument
+// push the new word into our new_array variable
+
+
+//Running out of time, but would refactor to put in rules for 1 through 10 min max
+// If I made a user interface it would prompt the user to enter a number 1 through 10 for new string length
+function wordGenerator(number) {
+  var alphabet = "abcdefghijklmnopqrstuvwxyz";
+  var new_word = "";
+
+  for (var i = 0; i < number; i++) {
+    new_word += alphabet[Math.floor(Math.random()*alphabet.length)];
+  }
+  return new_word;
+}
+
+function arrayGenerator(integer) {
+  var new_array = [];
+  for (var i = 0; i < integer; i++) {
+    new_array.push(wordGenerator(Math.ceil(Math.random()*10)));
+  }
+  return new_array
+}
+
+//Test code for release 2
+for (var i = 0; i < 10; i++) {
+  console.log(return_longest_string(arrayGenerator(3)));
+}
